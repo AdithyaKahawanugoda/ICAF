f4658a8a5ceeb40c1069a1fea968ce9b7ab95182
Adithya Kahawanugoda <adithyakahawanugoda@gmail.com>
2021-04-08 00:01:08 +0530

    complete frontend manual initial files has been added filled empty folders with fillempty.txt files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import "./App.css";
 
 const App = () => {
-  return (
-    <React.Fragment>
-      <div className="App">
-        <h1>GRID</h1>
-      </div>
-    </React.Fragment>
-  );
+  return <div>Hello</div>;
 };
 
 export default App;
