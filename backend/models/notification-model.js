e37d703b78159e04840568b5ea40ee971da8d728
Adithya Kahawanugoda <adithyakahawanugoda@gmail.com>
2021-05-22 11:34:06 +0530

    added notification channel model

---
+++ backend/models/notification-model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const NotificationChannelSchema = new mongoose.Schema({
+  subject: {
+    type: String,
+    required: true,
+  },
+  description: { type: String, required: true },
+  from: {
+    userid: { type: mongoose.Schema.Types.ObjectId },
+    userRole: { type: String, required: true },
+  },
+  to: {
+    userid: { type: mongoose.Schema.Types.ObjectId },
+    userRole: { type: String, required: true },
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const NotificationChannel = mongoose.model(
+  "notification",
+  NotificationChannelSchema
+);
+
+module.exports = NotificationChannel;
